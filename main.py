06ba12128ed797cf0c105e1d0d42582dbc8ba3cf
Dikean <dylan01aponte@gmail.com>
2024-12-21 17:50:02 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df1= pd.read_excel('./Database/Empleados.xlsx', sheet_name="Empleados")
+df2= pd.read_excel('./Database/Salarios.xlsx')
+
+df_resulante= pd.merge(df1, df2, on="Cedula")
+
+df_resulante.to_excel("./Resulante.xlsx", index=False)
+
